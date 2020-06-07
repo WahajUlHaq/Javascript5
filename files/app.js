74e4d895a033390462d8ec914fc98b2a1a45b858
WahajUlHaq <64356149+WahajUlHaq@users.noreply.github.com>
2020-06-07 14:49:47 +0500

    Add files via upload

---
+++ files/app.js
@@ -0,0 +1,82 @@
+
+// ALERTS CHAP  1
+
+alert("CHAPTER 1 Task Start From Here");// section 1 
+
+
+alert("Please! Enter A Valid Password TASK 1");// task 2
+
+alert("Welcome To JS LAND !" + '\n' + "HAPPY CODING!" + '\n' + "TASK 2");// task 3
+
+
+alert("Welcome To Js Land");// task 4
+
+alert("Happy Coding !")
+
+
+alert("Hello... I can Js through my browser console task no 5") //i do it in web console and printed a alert and it is task 5 
+
+
+
+
+var end1 = "Chap 1 END";
+alert(end1)
+
+// VARIABLE CHAP 2
+
+
+var username = "Wahaj Ul Haq";
+var myname = "WAHAJ UL HAQ STUDENT OF SAYLANI"; // task 1 chap 2
+alert(myname);
+
+
+
+var message = "HELLO WORLD";// task 2 chap 2
+
+alert(message);
+
+
+
+
+
+var studentBio1 = "Jhone Deo";// task 3 chap 2
+var studentBio3 = "15 Years Old";
+var studentBio4 = "Certified Developer";
+alert(studentBio1);
+alert(studentBio3);
+alert(studentBio4);
+
+
+
+
+alert("pizza" + '\n' + "pizz" + '\n' + "piz" + '\n' + "pi" + '\n' + "p");// task 4 chap 2
+
+
+
+
+
+var email = "example@example.com";// task 5 chap 2
+var text = "My email address";
+alert(text + " " + email);
+
+
+
+var A = "A smarter way to learn JavaScript";// task 6 chap 2
+alert(A);
+
+
+
+
+
+var bro = "Thanks Saylani I Can Write Also In Browser";// task 7 chap 2
+document.write(bro);
+
+
+// task 8 chap 2
+var sym = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(sym)
+
+
+
+var end2 = "Chap 2 END";
+alert(end2)
